6e7446113234338d98e9fbcb6056a33451bf111e
Kilua <itslukiee@gmail.com>
2024-02-20 22:47:45 +0800

    renaissance

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -3,26 +3,40 @@ require("mason").setup({
         icons = {
             package_installed = "✓",
             package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+            package_uninstalled = "✗",
+        },
+    },
 })
 
-require("mason-lspconfig").setup {
-    ensure_installed = { "pyright" },
-    require 'lspconfig'.pyright.setup {},
-    require 'lspconfig'.lua_ls.setup {},
-    require 'lspconfig'.emmet_ls.setup {},
-}
+require("mason-lspconfig").setup({
+    ensure_installed = { "pyright", "html", "cssls", "tsserver" },
+})
+
+require("lspconfig").pyright.setup({})
+require("lspconfig").lua_ls.setup({})
+require("lspconfig").emmet_ls.setup({})
+require("lspconfig").cssls.setup({})
+require("lspconfig").html.setup({})
+require("lspconfig").tsserver.setup({})
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.diagnostics.eslint,
+        -- null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
         -- null_ls.builtins.completion.spell,
     },
 })
 
-vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>F", function() vim.lsp.buf.format({ timeout_ms = 10000 }) end, {})
+
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--     pattern = "*",
+--     callback = function()
+--         -- You can include conditions here to only format certain file types
+--         -- For example: if vim.bo.filetype == "javascript" then
+--         vim.lsp.buf.format({ timeout_ms = 10000 })
+--         -- end
+--     end,
+-- })
